b53e952c168941d736e95e2829f6ceee04a10f5f
AhmadRiza <riza.public@gmail.com>
2020-02-28 20:49:56 +0700

    add kotlin lifecycle

--- app/src/main/java/riza/com/cto/MapsActivity.kt
+++ app/src/main/java/riza/com/cto/MapsActivity.kt
@@ -1,5 +1,6 @@
 package riza.com.cto
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -9,10 +10,15 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.Polygon
+import com.google.android.gms.maps.model.PolygonOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+
+    private var isDrawing = false
+    private var mPolygon : Polygon? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,5 +35,31 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        mMap.setOnMapClickListener {
+
+            if(isDrawing) addPolygonPoint(it)
+
+        }
+
     }
+
+    private fun addPolygonPoint(it: LatLng?) {
+        if(mPolygon == null) createPolygon()
+
+    }
+
+    private fun createPolygon() {
+
+        mPolygon = mMap.addPolygon(
+
+            PolygonOptions()
+                .fillColor(Color.CYAN)
+
+        )
+
+        mPolygon?.tag = "drawing"
+    }
+
+
 }
